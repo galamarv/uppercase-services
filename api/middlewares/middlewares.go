976e85417bd2e08ada5cfc4fbf334acc9e543b7f
Fattah Rizki Hizama L <fattah.rizki@gmail.com>
2021-10-14 15:38:35 +0700

    add auth

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,18 +1,20 @@
 package middlewares
 
 import (
+	"crypto/subtle"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func SetMainMiddleWares(e *echo.Echo) {
-	e.Use(serverHeader)
-}
-
-//Custom Middleware
-// ServerHeader middleware adds a `Server` header to the response.
-func serverHeader(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		c.Response().Header().Set("Custom-Header", "blah!!!")
-		return next(c)
-	}
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		// Be careful to use constant time comparison to prevent timing attacks
+		if subtle.ConstantTimeCompare([]byte(username), []byte("oval")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("KompasGramedia")) == 1 {
+			return true, nil
+		}
+		return false, echo.NewHTTPError(http.StatusUnauthorized, "username atau password salah")
+	}))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module uppercase
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.6.1
 )
